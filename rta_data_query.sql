16cd28869f0b5de8d93cf09951a7c575e6bc52cb
Joe Kiernan <joe@digitalfuelcapital.com>
2025-07-20 15:38:21 -0400

    moved static data files into dedicated folder

--- rta_data_query.sql
+++
@@ -1,6 +0,0 @@
-Select p.*, c.*, com.*
-from prices as p
-	left join competitor_catalog_map as ccm on p.catalog_id = ccm.catalog_id
-	left join competitors as com on ccm.competitor_id = com.id
-	left join catalog as c on p.catalog_id = c.id
-where com.site_id is not null
